c38c5f025237b969427f46a9ff6a96f9c8352317
José Hisse <jhisse@users.noreply.github.com>
2025-06-26 03:40:28 -0300

    refactor: switch SPARQL query execution from GET to POST method for better query handling

--- src/metabase/driver/sparql/execute.clj
+++ src/metabase/driver/sparql/execute.clj
@@ -10,7 +10,7 @@
 
 (def ^:private default-accept-header "application/json")
 
-;; Execute SPARQL queries against an endpoint
+;; Execute SPARQL queries against an endpoint using POST
 ;;
 ;; Parameters:
 ;;   endpoint - URL of the SPARQL endpoint
@@ -23,22 +23,28 @@
 ;;   On success: [true, response-body] where response-body is the parsed JSON response
 ;;   On failure: [false, error-message] with the error message as string
 ;;
-;; This function handles all HTTP communication with the SPARQL endpoint,
-;; including error handling, logging, and response parsing.
+;; This function handles all HTTP communication with the SPARQL endpoint
+;; using the POST method, which is robust for queries of any length.
 (defn execute-sparql-query
   [endpoint query options]
   (try
-    (let [params (cond-> {:query query}
-                   (:default-graph options) (assoc :default-graph-uri (:default-graph options)))
-          http-options (cond-> {:query-params params
-                                :headers {"Accept" default-accept-header}
-                                :throw-exceptions false}
-                         (:insecure? options) (assoc :insecure? true))]
-      (let [response (http/get endpoint http-options)]
-        (if (= 200 (:status response))
-          (let [body (json/decode+kw (:body response))]
-            [true body])
-          [false (str "SPARQL endpoint returned status: " (:status response) "\nBody: " (:body response))])))
+    (let [;; Options for the POST request
+          http-options (cond-> {:headers          {"Accept" default-accept-header}
+                                :throw-exceptions false
+                                :form-params      {:query query}}
+                         (:insecure? options) (assoc :insecure? true))
+
+          ;; The default-graph-uri is sent as a query parameter, even in a POST request
+          http-options (if-let [dg (:default-graph options)]
+                         (assoc http-options :query-params {:default-graph-uri dg})
+                         http-options)
+
+          response (http/post endpoint http-options)]
+
+      (if (= 200 (:status response))
+        (let [body (json/decode+kw (:body response))]
+          [true body])
+        [false (str "SPARQL endpoint returned status: " (:status response) "\nBody: " (:body response))]))
     (catch Exception e
       (log/error "Error executing SPARQL query:" (.getMessage e))
       [false (.getMessage e)])))
